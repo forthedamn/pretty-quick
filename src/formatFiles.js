5b17d4a0be51246993689c24cb404d9c4a8865d4
zhongguoxin <zhongguoxin@meituan.com>
2018-09-30 12:16:00 +0800

    to lint

--- src/formatFiles.js
+++ src/formatFiles.js
@@ -1,22 +1,41 @@
-import { readFileSync, writeFileSync } from 'fs';
-import { resolveConfig, format } from 'prettier';
-import { join } from 'path';
+'use strict'
+const tslint = require("tslint");
+const fs = require("fs");
+const path = require('path');
+const chalk = require('chalk');
+
+const {Configuration, Linter} = tslint;
 
 export default (directory, files, { config, onWriteFile } = {}) => {
-  for (const relative of files) {
-    const file = join(directory, relative);
-    const options = resolveConfig.sync(file, { config, editorconfig: true });
-    const input = readFileSync(file, 'utf8');
-    const output = format(
-      input,
-      Object.assign({}, options, {
-        filepath: file,
-      })
-    );
+  let hashError = false;
 
-    if (output !== input) {
-      writeFileSync(file, output);
-      onWriteFile && onWriteFile(relative);
+  const options = {
+      fix: false,
+      formatter: "json",
+      rulesDirectory: "customRules/",
+      formattersDirectory: "customFormatters/"
+  };
+
+  for (const relative of files) {
+    const file = path.join(directory, relative);
+    console.log('ffff', file);
+    
+    const fileName = relative;
+    const configurationFilename = "tslint.json";
+    const configuration = Configuration.findConfiguration(configurationFilename, fileName).results;
+    const fileContents = fs.readFileSync(file, "utf8");
+
+    const linter = new Linter(options);
+    linter.lint(fileName, fileContents, configuration);
+    const result = linter.getResult();
+    if (result && result.errorCount > 0) {
+      hashError = true;
+      for (let failure of result.failures) {
+        console.log(`ERROR: ${file}[${failure.startPosition.lineAndCharacter.line} ${failure.startPosition.position}]: ${failure.failure}`);
+      }
     }
+  }
+  if (hashError) {
+    process.exit(2);
   }
 };
